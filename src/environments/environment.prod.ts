9c8beacdc70b44d69ebdedbacef7c7a2dcf846e8
vule <levietvu1995@gmail.com>
2022-01-05 20:29:05 +0700

    config build prod gateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  gateway: 'http://localhost:3000/query',
+  gateway: 'https://pb-backend-app.herokuapp.com/query',
   callback: 'http://localhost:4200/callback',
   domain: '<YOUR_AUTH0_TENANT>.auth0.com',
   clientId: '<YOUR_AUTH0_APPLICATION_CLIENT_ID>',
